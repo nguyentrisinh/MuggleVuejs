1153a3b72430de7e7348b86aa92babc06655a16c
Sinhnt <nguyentrisinh0810@gmail.com>
2017-11-04 22:16:05 +0700

    Fix for listcompany

--- src/views/ListCompany/Company.vue
+++ src/views/ListCompany/Company.vue
@@ -1,79 +1,82 @@
 <template>
   <v-container>
-    <v-flex xs10 offset-xs1>
+    <V-layout>
+      <v-flex xs12 md12 lg8 offset-lg2>
+
+        <v-flex >
 
 
-      <!-- Từ khúc đầu đến card company -->
-      <h4 class="pl-2 pt-3">Company Reviews</h4>
-      <h6 class="pl-2">What are people saying about your company? Search now to check reviews and ratings.</h6>
-      <!--<button  id="btn">Search</button>-->
-      <div style="padding-top: 25px; padding-bottom: 25px">
-        <v-card-actions>
+          <!-- Từ khúc đầu đến card company -->
+          <h4 class="pl-2 pt-3">Company Reviews</h4>
+          <h6 class="pl-2">What are people saying about your company? Search now to check reviews and ratings.</h6>
+          <!--<button  id="btn">Search</button>-->
+          <div style="padding-top: 25px; padding-bottom: 25px">
+            <!--<v-card-actions>-->
 
-          <v-layout column >
-            <v-layout column >
-              <v-container fluid grid-list-md class="grey lighten-4">
-                <v-layout row wrap>
-                  <v-flex
-                    xs12 sm10 style="margin: auto"
-                  >
+              <v-layout column >
+                <v-layout column >
+                  <v-container fluid grid-list-md class="grey lighten-4">
+                    <v-layout row wrap>
+                      <v-flex
+                        xs12 sm10 style="margin: auto"
+                      >
 
-                    <input placeholder="  Tìm kiếm công việc thực tập" id="search" class="white" />
+                        <input placeholder="  Tìm kiếm công việc thực tập" id="search" class="white" />
 
 
 
-                  </v-flex>
-                  <v-flex xs12 sm2>
-                    <div>
-                      <v-btn color="primary" dark style="width: 100%; height: 50px;background-color: #ff9e34">
-                        <v-icon>search</v-icon>
-                        Search
-                      </v-btn>
-                    </div>
-                  </v-flex>
+                      </v-flex>
+                      <v-flex xs12 sm2>
+                        <div>
+                          <v-btn color="primary" dark style="width: 100%; height: 50px;background-color: #ff9e34; margin: auto;">
+                            <v-icon>search</v-icon>
+                            Search
+                          </v-btn>
+                        </div>
+                      </v-flex>
 
+                    </v-layout>
+                  </v-container>
                 </v-layout>
-              </v-container>
-            </v-layout>
-          </v-layout>
+              </v-layout>
 
-        </v-card-actions>
+            <!--</v-card-actions>-->
 
-      </div>
-      <div class="pl-2"><h4>Most popular</h4></div>
-      <h6 class="pl-2 pb-4">Companies most viewed by "Chất" Developers</h6>
-    </v-flex>
-    <v-flex xs10 offset-xs1>
+          </div>
+          <div class="pl-2"><h4>Most popular</h4></div>
+          <h6 class="pl-2 pb-4">Companies most viewed by "Chất" Developers</h6>
+        </v-flex>
+        <v-flex>
           <v-container fluid grid-list-md class="grey lighten-4">
             <v-layout row wrap>
 
               <v-flex xs12 sm6 md4>
-                  <v-layout  class="hvr-grow card">
-                    <v-flex>
-                      <v-card>
-                        <v-card-media  height="200px"  class="black&#45;&#45;text">
-                          <img src="./images/kms-technology-headline-photo-compress.png" />
-                        </v-card-media>
-                        <v-card-title primary-title>
-                          <div>
-                            <h3 class="headline mb-0 pb-1">Kangaroo Valley Safari</h3>
-                            <div ><v-icon class="grey--text pr-2 ">location_on</v-icon><span class="grey--text">Quận Tân Bình</span></div>
-                            <div><v-icon class="grey--text pr-2 ">perm_identity</v-icon><span class="grey--text text">Togather make perfect</span></div>
-                            <div class="pt-2">Agile & innovative global outsourcing...</div>
-                          </div>
-                        </v-card-title>
-                        <v-card-actions>
-                          <v-btn flat color="orange" class="white--text orange darken-2">
-                            <v-label>3.5</v-label>
-                            <v-icon class="white--text pl-2">stars</v-icon>
-                          </v-btn>
-                          <v-spacer></v-spacer>
-                          <v-btn  class="white--text success">3 Job</v-btn>
-                        </v-card-actions>
-                      </v-card>
-                    </v-flex>
-                  </v-layout>
-               </v-flex>
+                <v-layout  class="hvr-grow card">
+                  <v-flex>
+                    <v-card>
+                      <v-card-media  height="200px"  class="black&#45;&#45;text">
+                        <img src="./images/kms-technology-headline-photo-compress.png" />
+                      </v-card-media>
+                      <v-card-title primary-title>
+                        <div>
+                          <h3 class="headline mb-0 pb-1">Kangaroo Valley Safari</h3>
+                          <div ><v-icon class="grey--text pr-2 ">location_on</v-icon><span class="grey--text">Quận Tân Bình</span></div>
+                          <div><v-icon class="grey--text pr-2 ">perm_identity</v-icon><span class="grey--text text">Togather make perfect</span></div>
+                          <div class="pt-2">Agile & innovative global outsourcing...</div>
+                        </div>
+                      </v-card-title>
+                      <v-card-actions>
+                        <v-btn flat color="orange" class="white--text orange darken-2">
+                          <v-label>3.5</v-label>
+                          <v-icon class="white--text pl-2">stars</v-icon>
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn  class="white--text success">3 Job</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
 
               <v-flex xs12 sm6 md4>
                 <v-layout  class="hvr-grow card">
@@ -219,12 +222,15 @@
             </v-layout>
           </v-container>
 
-    </v-flex>
-    <v-flex xs10 offset-xs1>
-      <div class="text-xs-center ma" >
-        <v-btn large style="text-align: center; "  grey class="mt-5 mb-5">Show More Recommended Companys</v-btn>
-      </div>
-    </v-flex>
+        </v-flex>
+        <v-flex>
+          <div class="text-xs-center ma" >
+            <v-btn large style="text-align: center; "  grey class="mt-5 mb-5">Show More Recommended Companys</v-btn>
+          </div>
+        </v-flex>
+
+      </v-flex>
+    </V-layout>
   </v-container>
 
 </template>
@@ -234,10 +240,6 @@
 </script>
 
 <style>
-
-  .card{
-    padding: 4px;
-  }
   /* Grow */
   .hvr-grow {
     display: inline-block;
